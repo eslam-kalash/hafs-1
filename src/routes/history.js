01a0cf145971666c8e0524d9c2da18de35a028a0
karimqalash <kekoessam.ke@gmail.com>
2021-01-04 01:27:06 +0200

    setup

---
+++ src/routes/history.js
@@ -0,0 +1,8 @@
+// External dependencies
+import { createBrowserHistory } from 'history';
+
+// Create history
+const history = createBrowserHistory();
+
+// Export history
+export default history;
---
+++ src/routes/index.jsx
@@ -0,0 +1,24 @@
+// Routes/index.js
+// Top level react routing for the application
+
+// Created by: Karim Essam
+// Creation date: 1 January 2021
+
+// External dependencies
+import React from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
+
+// internal imports
+import Landing from './Landing';
+import history from './history';
+
+// Create a Router context and <Switch> component to render first matched <Route>
+export default () => {
+    return (
+        <Router history={history}>
+            <Switch>
+                <Route path="/" exact component={Landing} />
+            </Switch>
+        </Router>
+    );
+};
