575f712d5020a28a8dd97f407f749ad09adf6b9f
karimqalash <kekoessam.ke@gmail.com>
2021-01-21 21:51:00 +0200

    added classrom

--- src/routes/Dashboard/index.jsx
+++ src/routes/Dashboard/index.jsx
@@ -1,14 +1,19 @@
 // External dependencies
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import Navigation from './../../components/Navigation';
 import DashboardPage from './../../components/DashboardPage';
+import ClassroomPage from './../../components/ClassroomPage';
 import Footer from './../../components/Footer';
 
 const Dashboard = () => (
         <div className="page-container">
             <Navigation />
-            <DashboardPage />
+            <Switch>
+                <Route exact path='/dashboard' component={DashboardPage} />
+                <Route path='/dashboard/classroom' component={ClassroomPage} />
+            </Switch>
             <Footer />
         </div>
 );
